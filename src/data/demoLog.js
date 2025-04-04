a2931b8ce3a61ab0c867531047461e6e5aacaacf
Risa Niranjan Chaudhari <risa.chaudhari@gmail.com>
2025-04-05 01:56:07 +0530

    Added my feature

---
+++ src/data/demoLog.js
@@ -0,0 +1,17 @@
+const demoLog = [
+    {
+      id: 1,
+      user: "Ishika",
+      timestamp: "2025-04-03T12:00:00Z",
+      message: "Hello, how can I manage my workload?",
+    },
+    {
+      id: 2,
+      user: "Ishika",
+      timestamp: "2025-04-03T12:01:30Z",
+      message: "I sometimes struggle with focus.",
+    },
+  ];
+  
+  export default demoLog;
+  
